5bdd155cbc0c3c68e92624fee002cd49b89d3b51
dayanacavalcante <d.liria@gmail.com>
2023-12-07 18:50:54 +0000

    updated dashboard

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### Global Data on Sustainable Energy (2000-2019)
 
-Build a dasboard using the Dash and Plotly libraries to analyze trends in CO2 emissions, electricity from fossil fuels and renewable sources, and economic development of the 5 countries that emitted the most CO2 between the years 2000 and 2019.
+Build a dasboard using the Dash and Plotly libraries to analyze trends in CO₂ emissions, electricity from fossil fuels and renewable sources, and economic development of the 5 countries that emitted the most CO₂ between the years 2000 and 2019.
 
 
 Data from the analyzed countries can be viewed together or separately.
 
-![](./Chart/CO2_emissions_chart.jpg)
+![](./Chart/CO₂_emissions.jpg)
 
 Run the app with `dashboard.py`, read the file clean_data.csv, and visit the generated http in your web browser.    
 
--- Src/dashboard.py
+++ Src/dashboard.py
@@ -8,12 +8,13 @@ import plotly.graph_objs as go
 app = Dash(__name__)
 
 # Read data
-df = pd.read_csv("clean_data.csv")
+df = pd.read_csv("C:/Users/RenanSardinha/Documents/Dayana/Projects/Dashboard/Data/clean_data.csv")
 
-# Countries with the highest co2 emissions
+# Countries with the highest CO2 emissions
 average_co2_emission_by_country = df.groupby('Entity')['Value_co2_emissions_kt_by_country'].mean()
 top_5 = average_co2_emission_by_country.nlargest(5)
 filtered_df = df[df['Entity'].isin(top_5.index)]
+co2_str = "CO\u2082" 
 
 # Create charts and table
 fig = px.line(filtered_df, x="Year", y="Value_co2_emissions_kt_by_country", color="Entity") 
@@ -37,39 +38,40 @@ app.layout = html.Div(
         'padding': '22px' 
     },
     children=[
-        html.H1(children='Top 5 CO2 Emitting Countries from the years 2000 to 2019',style={'margin-left': '10px'}),
-        html.Span("Trends in CO2 emissions, electricity from fossil fuels and renewable sources, and economic development.", style={'margin-left': '10px','font-size': '18px'}),
-        html.H2(children='CO2 emissions in metric tons',style={'margin-left': '10px'}),
-    
-        dcc.Dropdown(options, value='All Countries', id='country_list', style={'width': '150px', 'margin-bottom': '15px'}),
+        html.H1(children=f'Top 5 {co2_str} Emitting Countries from the years 2000 to 2019',style={'margin-left': '10px'}),
+        html.Span(f"Trends in {co2_str} emissions, electricity from fossil fuels and renewable sources, and economic development.", style={'margin-left': '10px','font-size': '18px'}),
+            
+        dcc.Dropdown(options, value='All Countries', id='country_list', style={'width': '150px', 'margin-bottom': '15px', 'margin-top': '20px', 
+                                                                               'margin-left': '7px', 'font-size': '15px'}),
         
+        html.H2(children=f' {co2_str} emissions in metric tons',style={'margin-left': '10px', 'margin-bottom': '10px'}),
         dcc.Graph(
             id='emissions_co2',
             figure=fig,
         ),
         
-        html.H2(children='Electricity generated from renewable sources (hydro, solar, wind, etc.) in terawatt-hours',style={'margin-left': '15px'}),
+        html.H2(children='Electricity generated from renewable sources (hydro, solar, wind, etc.) in terawatt-hours',style={'margin-left': '15px', 'margin-bottom': '10px'}),
         dcc.Graph(
             id='use_of_renewable_energy',
             figure=fig_II,
             style={'backgroundColor': '#f2f2f2', 'padding': '20px'}
         ),
         
-        html.H2(children='Electricity generated from fossil fuels (coal, oil, gas) in terawatt-hours',style={'margin-left': '15px'}),
+        html.H2(children='Electricity generated from fossil fuels (coal, oil, gas) in terawatt-hours',style={'margin-left': '15px', 'margin-bottom': '10px'}),
         dcc.Graph(
             id='use_of_fossil_fuels_energy',
             figure=fig_III,
             style={'backgroundColor': '#f2f2f2', 'padding': '20px'}
         ),
         
-        html.H2(children='Gross domestic product (GDP) per person',style={'margin-left': '15px'}),
+        html.H2(children='Gross domestic product (GDP) per person',style={'margin-left': '15px', 'margin-bottom': '10px'}),
         dcc.Graph(
             id='gdp_per_capita',
             figure=fig_IV,
             style={'backgroundColor': '#f2f2f2', 'padding': '20px'}
         ),
         
-        html.H2(children='CO2 emissions and GDP per capita',style={'margin-left': '15px'}),
+        html.H2(children=f'{co2_str} emissions and GDP per capita',style={'margin-left': '15px', 'margin-bottom': '10px'}),
         dcc.Graph(
             id='gdp_per_capita_vs_co2',
             figure=fig_V,
@@ -90,11 +92,11 @@ app.layout = html.Div(
 def update_output(value):
     if value == "All Countries":
         fig = px.line(filtered_df, x="Year", y="Value_co2_emissions_kt_by_country", color="Entity")
-        fig.update_layout(paper_bgcolor='#f2f2f2')
+        fig.update_layout(paper_bgcolor='#f2f2f2', yaxis_title=f"{co2_str} emissions")
     else:
         filtered_table = df.loc[df['Entity']==value, :]
         fig = px.line(filtered_table, x='Year', y='Value_co2_emissions_kt_by_country', color='Entity')
-        fig.update_layout(paper_bgcolor='#f2f2f2')
+        fig.update_layout(paper_bgcolor='#f2f2f2', yaxis_title=f"{co2_str} emissions")
     return fig
 
 @app.callback(
@@ -133,11 +135,11 @@ def update_output_III(value):
 def update_output_IV(value):
     if value == "All Countries":
         fig_IV = px.line(filtered_df, x="Year", y="gdp_per_capita", color="Entity")
-        fig_IV.update_layout(paper_bgcolor='#f2f2f2')
+        fig_IV.update_layout(paper_bgcolor='#f2f2f2', yaxis_title="GDP per capita")
     else:
         filtered_table = filtered_df.loc[df['Entity']==value, :]
         fig_IV = px.line(filtered_table, x='Year', y='gdp_per_capita', color='Entity') 
-        fig_IV.update_layout(paper_bgcolor='#f2f2f2')
+        fig_IV.update_layout(paper_bgcolor='#f2f2f2', yaxis_title="GDP per capita")
     return fig_IV
 
 @app.callback(
@@ -147,11 +149,11 @@ def update_output_IV(value):
 def update_output_V(value):
     if value == "All Countries":
         fig_V = px.scatter(filtered_df, x='gdp_per_capita', y='Value_co2_emissions_kt_by_country', color='Entity')
-        fig_V.update_layout(paper_bgcolor='#f2f2f2')
+        fig_V.update_layout(paper_bgcolor='#f2f2f2', yaxis_title=f"{co2_str} emissions",xaxis_title="GDP per capita")
     else:
         filtered_table = filtered_df.loc[df['Entity']==value, :]
         fig_V = px.scatter(filtered_table, x='gdp_per_capita', y='Value_co2_emissions_kt_by_country', color='Entity')
-        fig_V.update_layout(paper_bgcolor='#f2f2f2')
+        fig_V.update_layout(paper_bgcolor='#f2f2f2', yaxis_title=f"{co2_str} emissions",xaxis_title="GDP per capita")
     return fig_V
 
 @app.callback(
